8247d35a535cb3377d3a0588d237a731a12539d9
pneal89 <pneal@memphis.edu>
2017-03-12 15:35:48 +0000

    course info, linking to homepage, some basic styling

---
+++ app/models/course.rb
@@ -0,0 +1,23 @@
+class Course < ApplicationRecord
+	validates :term, presence: true, numericality: { only_integer: true ,
+    message: "only allows numbers" }, length: { is: 6 }
+
+	validates :rubric, presence: true, inclusion: { in: %w(CHEM),
+    message: "%{value} must be a Chemistry course" }
+
+    validates :course_number, presence: true, numericality: { only_integer: true ,
+    message: "only allows numbers" }, length: { is: 4 }
+
+    #validates_uniqueness_of :section, scope: :term, message: "must be a unique term + section" }
+
+    validates :credit_hours, presence: true, numericality: {less_than: 5}
+
+    validates :course_type, presence: true, inclusion: { in: %w(lecture laboratory online independent\ study),
+    message: "%{value} not a valid type" }
+
+    validates :day, presence: true
+
+    validates :time, presence: true
+
+    validates :instructor, presence: true
+end
