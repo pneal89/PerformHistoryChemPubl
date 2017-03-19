6f5ae77cc11adc187ea0de5712aeebefba344409
Joseph Murphy <jmrphyii@memphis.edu>
2017-03-19 00:14:37 -0500

    clean up route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  devise_for :users
   resources :course_students
   resources :colleges
   resources :high_schools
@@ -8,14 +8,6 @@ Rails.application.routes.draw do
   resources :statistics
   resources :students
   resources :courses
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: 'test#index'
-
-  devise_for :users
-
   get 'users', to: 'users#index', as: 'admin_users'
+  root to: 'test#index'
 end
