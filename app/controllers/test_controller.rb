72782c05d9abde4f64a74ce47fe412626d91184a
Joseph Murphy <jmrphyii@memphis.edu>
2017-03-19 00:06:07 -0500

    misc files

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,4 +1,9 @@
 class TestController < ApplicationController
   def index
+    @students = Student.all
+    @student = Student.new
+    @instructors = Instructor.all
+    @courses = Course.all
+    @users = User.all
   end
 end
