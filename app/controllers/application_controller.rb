1db1baf668f5013c0315ad183319069c35568d45
Joseph Murphy <jmrphyii@memphis.edu>
2017-04-09 22:44:12 -0500

    fix login issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   layout :layout_by_resource
@@ -16,6 +17,10 @@ class ApplicationController < ActionController::Base
   end
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fname,:lname])
+  end
 
   def layout_by_resource
     if devise_controller?
