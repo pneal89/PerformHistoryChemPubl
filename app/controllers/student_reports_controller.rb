3a0d22618adca6ee703d9b013e5ec63ffa59c326
Joseph Murphy <jmrphyii@memphis.edu>
2017-05-02 14:04:52 -0500

    fix report

--- app/controllers/student_reports_controller.rb
+++ app/controllers/student_reports_controller.rb
@@ -2,10 +2,6 @@ class StudentReportsController < ApplicationController
   def attempts
     # @student_twice = CourseStudent.all.group_by(&:student_id).order('course_id')
     @students = Student.all
-    @report_data = []
-    @students.each do |s|
-      @courses = CourseStudent.where(student_id: s.id).group(:course_id).count
-      @report_data.push([s,@courses])
-    end
-  end
+    @courses = CourseStudent.where("course_id >= 2")
+end
 end
