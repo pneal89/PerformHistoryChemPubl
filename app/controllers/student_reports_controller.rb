9c0480da60fe99fb4f4e6d2dd9b1a6aabbbed194
Ben Murphy <bmmurphy@memphis.edu>
2017-05-02 14:10:11 -0500

    fixed report again

--- app/controllers/student_reports_controller.rb
+++ app/controllers/student_reports_controller.rb
@@ -2,6 +2,6 @@ class StudentReportsController < ApplicationController
   def attempts
     # @student_twice = CourseStudent.all.group_by(&:student_id).order('course_id')
     @students = Student.all
-    @courses = CourseStudent.where("course_id >= 2")
+    @courses = CourseStudent.where("attempt >= 2")
 end
 end
