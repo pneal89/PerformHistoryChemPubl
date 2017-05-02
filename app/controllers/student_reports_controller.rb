b8a77c0c17118f35080b5a63518a4fa8eedcb642
Joseph Murphy <jmrphyii@memphis.edu>
2017-05-02 00:08:03 -0500

    make it pretty

--- app/controllers/student_reports_controller.rb
+++ app/controllers/student_reports_controller.rb
@@ -2,6 +2,10 @@ class StudentReportsController < ApplicationController
   def attempts
     # @student_twice = CourseStudent.all.group_by(&:student_id).order('course_id')
     @students = Student.all
-    @student_second =
+    @report_data = []
+    @students.each do |s|
+      @courses = CourseStudent.where(student_id: s.id).group(:course_id).count
+      @report_data.push([s,@courses])
+    end
   end
 end
--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,5 +1,5 @@
 module StudentsHelper
   def course_name(id)
-    return 'CHEM ' + + Course.find(id).course_number.to_s
+    return 'CHEM '  + Course.find(id).course_number.to_s
   end
 end
