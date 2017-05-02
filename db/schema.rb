8d361b8d317af9f7bc212d44f98032674d14f8bd
Ben Murphy <bmmurphy@memphis.edu>
2017-05-02 12:54:53 -0500

    added per-semester grade/attempt tracking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322011153) do
+ActiveRecord::Schema.define(version: 20170502070025) do
 
   create_table "colleges", force: :cascade do |t|
     t.string   "city"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170322011153) do
     t.integer  "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "term"
     t.index ["course_id"], name: "index_course_students_on_course_id"
     t.index ["student_id"], name: "index_course_students_on_student_id"
   end
